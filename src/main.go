1002859829b8bb00c8cfcde2675b944eadffc32d
Awei <wiwi328122@gmail.com>
2022-03-11 15:51:28 +0800

    變更設定

--- src/main.go
+++ src/main.go
@@ -64,6 +64,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 
